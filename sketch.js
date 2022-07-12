c96b62aa07cf05dc66a812e8c789699e626e33b8
TARANSINGH18 <104207662+TARANSINGH18@users.noreply.github.com>
2022-07-12 18:21:15 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,71 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var ball,groundObj,leftSide,rightSide;
+var world;
+var radius = 70;
+
+function preload(){
+//find the bug in the below code
+	//dustbinImg = addImage("dustbin.png");
+	//paperImg = addImage("paper.png");
+
+}
+
+
+function setup() {
+	createCanvas(1600, 700);
+	rectMode(CENTER);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	var ball_options={
+		isStatic:false,
+		restitution:0.3,
+		density:0.4
+	}
+
+	ball = Bodies.circle(260,100,radius/2.6,ball_options);
+	World.add(world,ball);
+
+	ground=new Ground(width/2,670,width,20);
+	leftSide = new Ground(1100,600,10,120);
+	rightSide = new Ground(1270,600,10,120);
+	bottomSide = new Ground(1185,650,150,20);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+	background(200);
+	rectMode(CENTER);
+
+
+	ground.display();
+	leftSide.display();  
+	rightSide.display();
+	bottomSide.display();
+
+	
+	imageMode(CENTER);
+		//use image() command to add paper image to the ball
+image(,ball.position.x,ball.position.y,radius,radius);
+
+	// use image() command to add dustbin image in the canvas.
+	image(,1185, 570, 200,200);
+	
+
+}
+
+function keyPressed() {
+  	if (keyCode === UP_ARROW) {
+
+		Matter.Body.applyForce(ball,ball.position,{x:85,y:-85});
+    
+  	}
+}
